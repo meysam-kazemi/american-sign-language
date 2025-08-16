a431033be834a45db0a928317dbff155a1dfdfae
meysam-kazemi <meisam.daysu@gmail.com>
2025-08-16 13:24:11 +0330

    fix loading model on cpu

--- src/run_model.py
+++ src/run_model.py
@@ -5,11 +5,15 @@ import cv2 as cv
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.model_structure import CNN
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 TRAINED_MODEL_PATH = './models/asl_cnn_model.pth'
 
 model = CNN(num_classes=37)
 
-model.load_state_dict(torch.load(TRAINED_MODEL_PATH, weights_only=True))
+if device==device==torch.device('cpu'):
+    model.load_state_dict(torch.load(TRAINED_MODEL_PATH, weights_only=True, map_location=device))
+else:
+    model.load_state_dict(torch.load(TRAINED_MODEL_PATH, weights_only=True))
 
 
 print(model)
