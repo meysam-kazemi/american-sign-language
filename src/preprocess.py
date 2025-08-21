0830de541cbb154a9297ec35df6435a7a2f795ae
meysam-kazemi <meisam.daysu@gmail.com>
2025-08-21 13:35:06 +0330

    Draw landmarks for train data

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import pickle
+import cv2 as cv
 import torch
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.utils import load_config
@@ -7,12 +9,22 @@ from src.realtime_hand_landmarks import landmarkDetection
 
 config = load_config()
 
-train_dir = config.get('DATA', 'train_dir')
+TRAIN_DIR = config.get('DATA', 'train_dir')
+LANDMARKS_DIR = config.get("DATA", "landmarks_dir")
 
 landmark_detection = landmarkDetection(config)
-sign_names = os.listdir(train_dir)
+all_landmarks = []
+sign_names = os.listdir(TRAIN_DIR)
+os.makedirs("with_landmarks/", exist_ok=True)
 for sign_name in sign_names:
-    for image in os.listdir(os.path.join(sign_names, sign_name):
+    for i, image in enumerate(os.listdir(os.path.join(sign_names, sign_name))):
+        img = cv.imread(image, 1)
+        img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        landmarks = landmark_detection.detect(img_rgb)
+        landmard_detection.draw_with_landmark(img, landmarks.multi_hand_landmarks)
+        all_landmarks.append(landmarks)
+        cv.imwrite("dataset/with_landmarks/"+str(i)+".csv", img)
 
-        
 
+with open(LANDMARKS_DIR, 'wb') as f:
+    pickle.dump(all_landmarks, f)
