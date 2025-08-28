70821f8f571e15afec11824e24df01b8b9d8922a
meysam-kazemi <meisam.daysu@gmail.com>
2025-08-28 13:12:16 +0330

    edit save files

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pickle
 import cv2 as cv
+import math
 import torch
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from src.utils import load_config
@@ -22,6 +23,8 @@ sign_names = os.listdir(TRAIN_DIR)
 os.makedirs("dataset/with_landmarks/", exist_ok=True)
 os.makedirs(LANDMARKS_DIR, exist_ok=True)
 for sign_name in sign_names:
+    if sign_name=="nothing":
+        continue
     os.makedirs("dataset/with_landmarks/"+sign_name, exist_ok=True)
     image_paths = os.listdir(os.path.join(TRAIN_DIR, sign_name))
     for i, image_path in enumerate(image_paths):
@@ -29,11 +32,12 @@ for sign_name in sign_names:
         img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         landmarks = landmark_detection.detect(img_rgb)
         landmark_detection.draw_with_landmark(img, landmarks.multi_hand_landmarks)
-        all_landmarks.append(landmarks)
+        all_landmarks.append(landmarks.multi_hand_landmarks)
         cv.imwrite(f"dataset/with_landmarks/{sign_name}/{str(i)}.png", img)
-        loading = int(20*i/len(image_paths)) + 1
-        print(f'{"="*loading:<20}|{loading}%', end='\r')
-    print()
+        loading = (50*i/len(image_paths)) + 1
+        print(f'{sign_name:<5}:[{"="*math.ceil(loading):<50}] {loading:.2f}%', end='\r')
+    print(f'Saving {sign_name} in {LANDMARKS_DIR+sign_name+".pkl"} ...', end='\r')
+    print(f'Saved {sign_name} in {LANDMARKS_DIR+sign_name+".pkl"} ✔\n')
     save_landmarks(all_landmarks, landmarks_dir=LANDMARKS_DIR+sign_name+".pkl")
 
 
